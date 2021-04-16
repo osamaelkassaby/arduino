c4c30f31034cdeee2ef8f017789a70f28ceeff55
osamaelkassaby1 <62217352+osamaelkassaby1@users.noreply.github.com>
2021-04-17 00:48:17 +0200

    Add files via upload

---
+++ sketch_apr16a.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+int i = 0;
+LiquidCrystal lcd(8,9,4,5,6,7);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Loding ....");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.home();
+  lcd.setCursor(0,1);
+  i++;
+ // lcd.print("Loning .....");
+  delay(400);
+ if(i == 20){
+  lcd.print("01555570509");
+  lcd.setCursor(0,0);
+  lcd.print("osamaelkassaby");
+
+  }
+  
+  
+
+}
